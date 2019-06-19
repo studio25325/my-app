45b7305d5379767e69f9930553b7bf1d431097f8
studio25325 <studio25325@gmail.com>
2019-06-19 16:11:50 +0900

    動作確認

---
+++ scheduler/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Schedule
+
+admin.site.register(Schedule)
+
+# Register your models here.
---
+++ scheduler/views.py
@@ -0,0 +1,15 @@
+from django.views import generic
+from .models import Schedule
+from django.utils import timezone
+from . import mixins
+
+
+class MonthCalendar(mixins.MonthCalendarMixin, generic.TemplateView):
+    """月間カレンダーを表示するビュー"""
+    template_name = 'scheduler/month.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        return context
