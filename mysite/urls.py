c4d3f7ff6a412626e8d2b200315f48b6c59740b3
studio25325 <studio25325@gmail.com>
2019-06-19 16:51:09 +0900

    tt

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('scheduler/', include('scheduler.urls')),
+    path('tt/', include('tt.urls')),
 ]
