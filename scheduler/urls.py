b211f5428cce465d0c3f745a4d39dff14d67bead
studio25325 <studio25325@gmail.com>
2019-06-25 15:40:05 +0900

    センタリング

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'scheduler'
 
 urlpatterns = [
-    path('', views.MonthCalendar.as_view(), name='month'),
+    #path('', views.MonthCalendar.as_view(), name='month'),
+    path('', views.WeekWithScheduleCalendar.as_view(), name='week_with_schedule'),
     path('month/<int:year>/<int:month>/', views.MonthCalendar.as_view(), name='month'),
     path('week/', views.WeekCalendar.as_view(), name='week'),
     path('week/<int:year>/<int:month>/<int:day>/', views.WeekCalendar.as_view(), name='week'),
